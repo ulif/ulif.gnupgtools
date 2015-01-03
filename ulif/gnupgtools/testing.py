a382ff97321bb0e96ac3740b9fdd7c886bab4c4a
ulif <uli@gnufix.de>
2015-01-03 14:48:48 +0100

    A module for testing support.

---
+++ ulif/gnupgtools/testing.py
@@ -0,0 +1,13 @@
+#
+# Helpers for testing
+#
+def doctest_setup():
+    """Setup env for complex doctests.
+    """
+    pass
+
+
+def doctest_teardown():
+    """Clean up env after complex doctests.
+    """
+    pass
