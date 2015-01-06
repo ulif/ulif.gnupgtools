0a1c5bf8b2637dc0360374e46bf88e2ebcdb35de
ulif <uli@gnufix.de>
2015-01-06 05:47:11 +0100

    Add test for get_secret_keys_output.

--- tests/test_export_master_key.py
+++ tests/test_export_master_key.py
@@ -2,7 +2,9 @@ import os
 import shutil
 import tempfile
 import unittest
-from ulif.gnupgtools.export_master_key import main, greeting, VERSION
+from ulif.gnupgtools.export_master_key import (
+    main, greeting, VERSION, get_secret_keys_output,
+    )
 
 
 class TestGPGExportMasterKeyTests(unittest.TestCase):
@@ -22,6 +24,13 @@ class TestGPGExportMasterKeyTests(unittest.TestCase):
         if os.path.exists(self.temp_dir):
             shutil.rmtree(self.temp_dir)
 
+    def create_sample_gnupg_home(self, name):
+        # create a gnupg sample config in self.gnupg_home
+        # name must be one of the subdirs in `gnupg-samples/`.
+        sample_home = os.path.join(
+            os.path.dirname(__file__), 'gnupg-samples', name)
+        shutil.copytree(sample_home, self.gnupg_home)
+
     def test_main_exists(self):
         # the main function exists
         assert main is not None
@@ -29,6 +38,21 @@ class TestGPGExportMasterKeyTests(unittest.TestCase):
     def test_version(self):
         # we can get a version string
         assert VERSION is not None
+
+    def test_get_secret_keys_output(self):
+        # we can get secret keys via gpg commandline tool
+        self.create_sample_gnupg_home('one-secret')
+        out, err = get_secret_keys_output()
+        assert err is None
+        assert out[-198:] == (
+            "/gnupghome/secring.gpg\n"
+            "------------------------------------\n"
+            "sec   2048R/16FD1DE8 2015-01-06\n"
+            "uid                  Gnupg Testuser (no real person) "
+            "<gnupg@example.org>\n"
+            "ssb   2048R/75DD62A6 2015-01-06\n"
+            "\n"
+            )
 
 
 def test_greeting(capsys):
