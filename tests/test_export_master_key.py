c4ba66a85b44471c5a2009212d3575c30779f6b5
ulif <uli@gnufix.de>
2015-01-14 05:13:39 +0100

    Split fixtures.

--- tests/test_export_master_key.py
+++ tests/test_export_master_key.py
@@ -41,15 +41,12 @@ def mock_input(request):
     return mocker
 
 
-class GnuPGHomeCreator(object):
+class WorkDirCreator(object):
 
     def __init__(self):
         self.temp_dir = tempfile.mkdtemp()
         self.workdir = os.path.join(self.temp_dir, 'work')
-        self.gnupg_home = os.path.join(self.temp_dir, 'gnupghome')
-        self._old_env = os.environ.copy()
         self._old_cwd = os.getcwd()
-        os.environ['GNUPGHOME'] = self.gnupg_home
         os.mkdir(self.workdir)
         os.chdir(self.workdir)
 
@@ -57,8 +54,20 @@ class GnuPGHomeCreator(object):
         os.chdir(self._old_cwd)
         if os.path.exists(self.temp_dir):
             shutil.rmtree(self.temp_dir)
+
+
+class GnuPGHomeCreator(WorkDirCreator):
+
+    def __init__(self):
+        super(GnuPGHomeCreator, self).__init__()
+        self.gnupg_home = os.path.join(self.temp_dir, 'gnupghome')
+        self._old_env = os.environ.copy()
+        os.environ['GNUPGHOME'] = self.gnupg_home
+
+    def tear_down(self):
         os.environ.clear()
         os.environ.update(self._old_env)
+        super(GnuPGHomeCreator, self).tear_down()
 
     def create_sample_gnupg_home(self, name):
         # create a gnupg sample config in self.gnupg_home
@@ -66,6 +75,13 @@ class GnuPGHomeCreator(object):
         sample_home = os.path.join(
             os.path.dirname(__file__), 'gnupg-samples', name)
         shutil.copytree(sample_home, self.gnupg_home)
+
+
+@pytest.fixture(scope="function")
+def work_dir_creator(request):
+    creator = WorkDirCreator()
+    request.addfinalizer(creator.tear_down)
+    return creator
 
 
 @pytest.fixture(scope="function")
