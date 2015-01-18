a9b1a4148e5390bbd2d7f443a451ba963845d88a
ulif <uli@gnufix.de>
2015-01-18 19:53:25 +0100

    pep8.

--- ulif/gnupgtools/export_master_key.py
+++ ulif/gnupgtools/export_master_key.py
@@ -91,8 +91,8 @@ def greeting():
     """
     print(
         ("gpg-export-master-key.py %s; Copyright (C) 2014 Uli Fouquet. "
-           "This is free software: you are free to change and redistribute "
-           "it. There is NO WARRANTY, to the extent permitted by law. "
+         "This is free software: you are free to change and redistribute "
+         "it. There is NO WARRANTY, to the extent permitted by law. "
          ) % VERSION
         )
 
