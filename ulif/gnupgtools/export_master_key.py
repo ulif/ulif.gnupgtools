f69fcb2f1f6ebf8bd2e42154574b2e02c85a9f60
ulif <uli@gnufix.de>
2015-01-24 11:30:28 +0100

    Add copyright notice.

--- ulif/gnupgtools/export_master_key.py
+++ ulif/gnupgtools/export_master_key.py
@@ -1,15 +1,31 @@
-# Create key files needed for exporting GPG subkeys to other machines.
 #
-# Exports all public and secret keys of a chosen primary keypair, including
-# subkeys (private and public parts) bound to this key.
+#    ulif.gnupgtools -- gnupg made less complex
+#    Copyright (C) 2015  Uli Fouquet
 #
-# The three resulting files contain the public keys  (<key-id>.pub),
-# all secret keys (<key-id>.priv) and the private keys except the
-# primary secret key (<key-id>.subkeys).
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-# *Before* running this script you must create additional subkeys.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+"""Create key files needed for exporting GPG subkeys to other machines.
+
+ Exports all public and secret keys of a chosen primary keypair, including
+ subkeys (private and public parts) bound to this key.
+
+ The three resulting files contain the public keys  (<key-id>.pub),
+ all secret keys (<key-id>.priv) and the private keys except the
+ primary secret key (<key-id>.subkeys).
+
+ *Before* running this script you must create additional subkeys.
+"""
 import argparse
 import grp
 import os
