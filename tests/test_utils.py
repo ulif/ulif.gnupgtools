9ff4ae584fdd5a7347ced475ad8302c73b0945ac
ulif <uli@gnufix.de>
2015-02-03 15:12:24 +0100

    Make sure, /bin/echo exists.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,11 @@
 # Tests for ulif.gnupgtools.utils module
+import os
 import pytest
 from ulif.gnupgtools.utils import execute
 
 
+@pytest.mark.skipif(
+    not os.path.exists('/bin/echo'), reason="needs /bin/echo")
 def test_execute():
     # we can execute commands (w/o shell)
     cmd = ["/bin/echo", 'Hello $PATH']
