e9b8f2fe6e26ab3d2976e2d8c9f9875ffa2afdd5
ulif <uli@gnufix.de>
2015-02-03 15:06:28 +0100

    Add test for execute().

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,10 @@
 # Tests for ulif.gnupgtools.utils module
 import pytest
+from ulif.gnupgtools.utils import execute
+
+
+def test_execute():
+    # we can execute commands (w/o shell)
+    cmd = ["/bin/echo", 'Hello $PATH']
+    out, err = execute(cmd)
+    assert out == b'Hello $PATH\n'
