42a4becf7555941615990c628f7d2cae10aeef51
ulif <uli@gnufix.de>
2015-02-07 01:36:34 +0100

    Add tests for tarfile contextmanager.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 # Tests for ulif.gnupgtools.utils module
 import os
 import pytest
-from ulif.gnupgtools.utils import execute, get_tmp_dir
+import tarfile
+from ulif.gnupgtools.utils import execute, get_tmp_dir, tarfileopen
 
 
 @pytest.mark.skipif(
@@ -30,3 +31,37 @@ def test_get_tmp_dir_w_exc():
             assert os.path.exists(d)
             raise Exception('Intented Exception')
     assert not os.path.exists(d)
+
+
+def test_tarfileopen(work_dir_creator):
+    # we can open tarfiles with contextmanager, also in Python 2.6
+    file1 = os.path.join(work_dir_creator.workdir, 'file1')
+    file2 = os.path.join(work_dir_creator.workdir, 'file2')
+    open(file1, 'w').write('file1')
+    open(file2, 'w').write('file2')
+    tmp_tar = None
+    with tarfileopen('sample.tar.gz', 'w:gz') as tar:
+        tar.add('file1')
+        tar.add('file2')
+        tmp_tar = tar
+    assert tmp_tar.closed is True
+    tar = tarfile.open('sample.tar.gz', 'r:gz')
+    member_names = [x.name for x in tar.getmembers()]
+    tar.close()
+    assert sorted(member_names) == ['file1', 'file2']
+
+
+def test_tarfileopen_w_exc(work_dir_creator):
+    # tar files are closed in case of exceptions
+    file1 = os.path.join(work_dir_creator.workdir, 'file1')
+    file2 = os.path.join(work_dir_creator.workdir, 'file2')
+    open(file1, 'w').write('file1')
+    open(file2, 'w').write('file2')
+    tmp_tar = None
+    with pytest.raises(OSError):
+        with tarfileopen('sample.tar.gz', 'w:gz') as tar:
+            tmp_tar = tar
+            tar.add('file1')
+            tar.add('file2')
+            tar.add('file3')
+    assert tmp_tar.closed is True
